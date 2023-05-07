160c7aebce07496dbe10aa615271bc732d6f6598
Jack <1731198425@qq.com>
2023-05-07 16:26:55 +0800

    完成了home静态页面的开发

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入三级联动组件
+import TypeNav from '@/views/Home/TypeNav/TypeNav.vue'
+
+// 全局注册三级联动组件
+// 使用TypeNav.name获取到组件的名字
+Vue.component(TypeNav.name, TypeNav)
 
 Vue.config.productionTip = false
 
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>我是Home组件</div>
+  <div>
+    <!-- 因为已经全局注册了TypeNav组件,所以不需要再局部import引入三级联动组件 -->
+    <TypeNav />
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
-export default {
+import ListContainer from '@/views/Home/ListContainer/ListContainer.vue'
+import Recommend from '@/views/Home/Recommend/Recommend.vue'
+import Rank from '@/views/Home/Rank/Rank.vue'
+import Like from '@/views/Home/Like/Like.vue'
+import Floor from '@/views/Home/Floor/Floor.vue'
+import Brand from '@/views/Home/Brand/Brand.vue'
 
+export default {
+  name: 'Home',
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
